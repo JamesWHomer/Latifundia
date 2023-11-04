b1bfd3ebf46cee2ea1c7c1ceb26ca181350fb7fe
James Homer <jameswatsonhomer@gmail.com>
2023-11-04 21:25:59 +0100

    Create CityStateManager shell

---
+++ src/main/java/net/uber/latifundia/CityStateManager.java
@@ -0,0 +1,64 @@
+package net.uber.latifundia;
+
+import net.uber.latifundia.claimmanagement.WorldTree;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class CityStateManager {
+
+    private final JavaPlugin plugin;
+    private final Map<UUID, CityState> cityStateMap = new HashMap<>();
+
+    public CityStateManager(JavaPlugin plugin) {
+
+        this.plugin = plugin;
+
+    }
+
+    public CityState getCityState(UUID cuuid) {
+
+        return null;
+
+    }
+
+    public void createCityState(String name, Player creator) {
+
+
+
+    }
+
+    public void setCityState(CityState cityState) {
+
+
+
+    }
+
+    public void loadAllCityStates() {
+
+
+
+    }
+
+    public CityState loadCityState(UUID cuuid) {
+
+        return null;
+
+    }
+
+    public void saveCityState(CityState cityState) {
+
+
+
+    }
+
+    public void saveAllCityStates(CityState cityState) {
+
+
+
+    }
+
+}
